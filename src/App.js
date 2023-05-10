4e63897119ae7396bd6477287fdda3de0ce6b8fe
taylor-nurihaus <taylorla@gmail.com>
2023-05-10 17:23:26 +0900

    ➖ 1.0 initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>github test2</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
