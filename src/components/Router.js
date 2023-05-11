d27d7e37552c4ee960d033a6a9eaaa77f7643e62
taylor-nurihaus <wktaylorla@gmail.com>
2023-05-11 18:02:02 +0900

    🚧 routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-import Home from "../routes/Home";
-import Auth from "../routes/Auth";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/" element={<Home userObj={userObj} />} />
+            <Route exact path="/profile" element={<Profile />} />
           </>
         ) : (
           <Route exact path="/" element={<Auth />} />
         )}
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </Router>
   );
