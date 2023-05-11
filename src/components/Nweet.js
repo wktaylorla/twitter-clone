45f8375f2ec7a31661a9d78e09c02838466ed4f9
taylor-nurihaus <wktaylorla@gmail.com>
2023-05-11 18:04:12 +0900

    🚧 main tweet screen in progress : write / delete / update / read tweet

---
+++ src/components/Nweet.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+
+import { dvService } from "fbase";
+
+export default function Nweet({ nweetObj, isOwner }) {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) await deleteDoc(doc(dvService, "nweets", nweetObj.id));
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(doc(dvService, "nweets", nweetObj.id), {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              onChange={onChange}
+              required
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  orderBy,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+import { dvService } from "fbase";
+
+import Nweet from "components/Nweet";
+
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(dvService, "nweets"),
+      orderBy("createdAt", "desc")
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const nweetArray = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dvService, "nweets"), {
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            nweetObj={nweet}
+            key={nweet.id}
+            isOwner={userObj.uid === nweet.creatorId}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
